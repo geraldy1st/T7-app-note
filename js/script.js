6a958e469890535e59f8f2bb00018652111c0b4d
Geraldy <geraldy.leondas@gmail.com>
2017-11-17 18:49:29 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+$(function(){
+	$("#akuma").click(function(){
+        $(".akuma").toggle(200);
+	});
+});
+$(function(){
+	$("#alisa").click(function(){
+		$(".alisa").toggle(200);
+	});
+});
+$(".option").hide();
+
